0c3133e84cdea1bbb03494ba7e73c763f7e336c1
Prof. Daniel Callegari <daniel.callegari@pucrs.br>
2024-03-22 18:28:05 -0300

    Adicionei o fim do programa

--- App.java
+++ App.java
@@ -2,5 +2,7 @@ public class App {
   public static void main (String[] args) {
     // Meu primeiro programa
     System.out.println("Hello, everyone!");
+    System.out.println("Fim do programa!");
+    
   }
 }
